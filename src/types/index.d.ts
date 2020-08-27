376e2608fddac12fb5534c935461f8c5610abd79
kino90 <kinovale@hotmail.com>
2020-08-27 10:04:02 +0200

    feat(types): Add TypeScript types

---
+++ src/types/index.d.ts
@@ -0,0 +1,8 @@
+declare module "filepond-plugin-file-encode" {
+  const FilePondPluginFileEncode: FilePondPluginFileEncodeProps;
+  export interface FilePondPluginFileEncodeProps {
+    /** Enable or disable file encode. */
+    allowFileEncode?: boolean;
+  }
+  export default FilePondPluginFileEncode;
+}
