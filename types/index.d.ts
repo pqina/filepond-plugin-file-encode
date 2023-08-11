a3b41481fea0dc7144017ac2bd589a6335950276
Jake Keuhlen <jak1214@gmail.com>
2023-08-11 17:58:37 -0600

    Add missing method types to index.d.ts

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,9 +1,14 @@
 // @ts-ignore
-import { FilePondOptions } from "filepond";
+import { FilePondFile, FilePondOptions } from "filepond";
 
 declare module "filepond" {
   export interface FilePondOptions {
     /** Enable or disable file encode. */
     allowFileEncode?: boolean;
+  }
+
+  export interface FilePondFile {
+    getFileEncodeBase64String(): string;
+    getFileEncodeDataURL(): string;
   }
 }
